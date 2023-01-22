59991977a3342e4a69a717f554d7a4c9fd1717b0
Oliver Praesto <oliver.praesto@gmail.com>
2023-01-23 00:16:40 +0100

    - adds seo and image height/width

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,11 +4,16 @@
 	export let data: PageServerData;
 </script>
 
+<svelte:head>
+	<title>Notion CMS</title>
+	<meta name="description" content="Images of cats from a Notion CMS Database" />
+</svelte:head>
+
 <div class="grid">
 	{#each data.images as image}
 		<div>
 			<article>
-				<img src={image.url} alt={image.title} />
+				<img src={image.url} alt={image.title} height="250" width="250" />
 				<footer>
 					<hgroup>
 						<h2>{image.title}</h2>
