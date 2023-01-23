6e6c4023f559b63d33c82e4733f08e47da50f908
Oliver Praesto <oliver.praesto@gmail.com>
2023-01-23 09:04:06 +0100

    - refactors

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -9,23 +9,19 @@ type CatImage = {
   created: string;
 };
 
-// export const prerender = true;
 const notion = new Client({ auth: NOTION_API_KEY });
 
 export const load: PageServerLoad = async () => {
-  const results: CatImage[] = [];
   const data = await notion.databases.query({
     database_id: NOTION_DB_ID
   });
 
-  data.results.forEach((image) => {
-    const mappedProps: CatImage = {
+  const results: CatImage[] = data.results.map((image) => {
+    return {
       url: (<any>image).properties.file.files[0].file.url,
       title: (<any>image).properties.Name.title[0].plain_text,
       created: (<any>image).created_time
     };
-
-    results.push(mappedProps);
   });
 
   return { images: results };
